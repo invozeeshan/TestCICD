5fd1c68dc5e567d240d40ee223b1bf88d3bb12b3
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-04 17:44:53 +0500

    update password

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -11,11 +11,11 @@ mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 cp ./.github/secrets/match_appstore_TextApp_profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/match_appstore_TextApp_profile.mobileprovision
 
-security create-keychain -p "1234" build.keychain
-security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "1234" -A
+security create-keychain -p $1234 build.keychain
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P $1234 -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "1234" ~/Library/Keychains/build.keychain
+security unlock-keychain -p $1234 ~/Library/Keychains/build.keychain
 
 security set-key-partition-list -S apple-tool:,apple: -s -k "1234" ~/Library/Keychains/build.keychain
