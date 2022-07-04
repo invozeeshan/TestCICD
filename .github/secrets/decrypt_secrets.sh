6d0db90910eb00d35fe85836b7de902f9787a450
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-04 14:17:20 +0500

    change app secrets

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -2,8 +2,17 @@
 #!/bin/sh
 set -eo pipefail
 
+
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/match_appstore_TextApp_profile.mobileprovision ./.github/secrets/match_appstore_TextApp_profile.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+
+mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+
+
+cp ./.github/secrets/match_appstore_TextApp_profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/match_appstore_TextApp_profile.mobileprovision
+
 security create-keychain -p "" build.keychain
-security import .github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+security import ./.github/secrets/Certificates.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
 security list-keychains -s ~/Library/Keychains/build.keychain
 security default-keychain -s ~/Library/Keychains/build.keychain
