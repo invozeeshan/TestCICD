f754909c7bdf99baa7331671b72723f94710dc39
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-04 15:13:14 +0500

    change workspace to project

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace TestApp.xcodeproj \
+xcodebuild -project TestApp.xcodeproj \
             -scheme TestApp\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
