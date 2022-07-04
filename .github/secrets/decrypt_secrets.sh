d2c2ebc94fe9b5fdb359e451bc257869d264bdb7
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-05 02:25:16 +0500

    only print passphrase

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -3,19 +3,19 @@
 set -eo pipefail
 
 echo "$IOS_KEYS"
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/CICD_Test__Release.mobileprovision.mobileprovision ./.github/secrets/CICD_Test__Release.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/HippocratesTech_Release.p12 ./.github/secrets/HippocratesTech_Release.p12.gpg
+#gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/CICD_Test__Release.mobileprovision.mobileprovision ./.github/secrets/CICD_Test__Release.mobileprovision.gpg
+#gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/HippocratesTech_Release.p12 ./.github/secrets/HippocratesTech_Release.p12.gpg
 
-mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
+#mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
 
-cp ./.github/secrets/CICD_Test__Release.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/CICD_Test__Release.mobileprovision.mobileprovision
+#cp ./.github/secrets/CICD_Test__Release.mobileprovision.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/CICD_Test__Release.mobileprovision.mobileprovision
 
-security create-keychain -p "" build.keychain
-security import ./.github/secrets/HippocratesTech_Release.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
+#security create-keychain -p "" build.keychain
+#security import ./.github/secrets/HippocratesTech_Release.p12 -t agg -k ~/Library/Keychains/build.keychain -P "" -A
 
-security list-keychains -s ~/Library/Keychains/build.keychain
-security default-keychain -s ~/Library/Keychains/build.keychain
-security unlock-keychain -p "" ~/Library/Keychains/build.keychain
+#security list-keychains -s ~/Library/Keychains/build.keychain
+#security default-keychain -s ~/Library/Keychains/build.keychain
+#security unlock-keychain -p "" ~/Library/Keychains/build.keychain
 
-security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
+#security set-key-partition-list -S apple-tool:,apple: -s -k "" ~/Library/Keychains/build.keychain
