b9565cda994f4d4b7045c298daaad52b9d09350c
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-04 15:19:11 +0500

    change the scheme

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -project TestApp.xcodeproj \
-            -scheme TestApp\ iOS \
+            -scheme TestApp
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
