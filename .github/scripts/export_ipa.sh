6951fb40375cb25e291466d24fd5499e8548d947
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-05 16:13:12 +0500

    export options plist file path changed

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 xcodebuild -archivePath $PWD/build/TestApp.xcarchive \
-            -exportOptionsPlist TestCICD/TestApp\ iOS/exportOptions.plist \
+            -exportOptionsPlist TestApp/exportOptions.plist \
             -exportPath $PWD/build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
