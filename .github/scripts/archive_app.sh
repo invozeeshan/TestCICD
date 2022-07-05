aaf6c5d5f5f556c732d6653de8647708979ec49b
Zeeshan Ahmed <zeeshan.ahmed@invozone.com>
2022-07-05 16:00:43 +0500

    archive script modified

--- .github/scripts/archive_app.sh
+++ .github/scripts/archive_app.sh
@@ -2,14 +2,14 @@
 
 set -eo pipefail
 
-xcodebuild -project TestApp.xcodeproj -scheme TestApp \
-        -archivePath build/TestApp.xcarchive archive \
-        PROVISIONING_PROFILE="$uuid" CODE_SIGN_IDENTITY="HippocratesTech_Release"
+#xcodebuild -project TestApp.xcodeproj -scheme TestApp \
+#        -archivePath build/TestApp.xcarchive archive \
+#        PROVISIONING_PROFILE="$uuid" CODE_SIGN_IDENTITY="HippocratesTech_Release"
 
 
-#xcodebuild -project TestApp.xcodeproj \
-#            -scheme TestApp \
-#            -sdk iphoneos \
-#            -configuration AppStoreDistribution \
-#            -archivePath $PWD/build/TestApp.xcarchive \
-#            clean archive | xcpretty
+xcodebuild -project TestApp.xcodeproj \
+            -scheme TestApp \
+            -sdk iphoneos \
+            -configuration AppStoreDistribution \
+            -archivePath $PWD/build/TestApp.xcarchive \
+            clean archive | xcpretty
